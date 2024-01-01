23b7cda34b925601d7e7b87b41f3289df87eb0e5
Jumah Muhamad <jumahtech@gmail.com>
2024-01-02 00:26:44 +0100

    edited te hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
-// import React from 'react'
 import "./Hero.css";
+import carousel1 from "../../assets/carousel-1.jpg";
 
 const Hero = () => {
   return (
@@ -7,73 +7,96 @@ const Hero = () => {
       <div
         id="carouselExampleAutoplaying"
         className="carousel slide"
-        data-bs-ride="carousel1"
+        data-bs-ride="carousel"
       >
-        <div className="carousel-inner h-75">
-          <div className="carousel-item active h-100" data-bs-interval="10000">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={carousel1} className="d-block w-100" alt="..." />
             <div className="carousel-caption">
               <h5 className="fw-bold">GLOBAL DIGITAL CAMPUS</h5>
-              <p className="first_subtext fw-bold text-white">
+              <p className="first_subtext fw-bold">
                 EMPOWERING FUTURE LEADERS.
               </p>
               <a href="#">
-                <button
-                  type="submit"
-                  className="submit d-block text-center m-auto"
-                >
+                <button type="submit" className="submit d-block m-auto">
                   Your experience feedback
                 </button>
               </a>
               <a href="#">
                 <button type="submit" className="notice mt-3">
+                  {" "}
                   Notice board
                 </button>
               </a>
             </div>
           </div>
-          <div className="carousel-item h-100" data-bs-interval="2000">
+          <div className="carousel-item">
+            <img src={carousel1} className="d-block w-100" alt="img" />
             <div className="carousel-caption">
               <h5 className="fw-bold">GLOBAL DIGITAL CAMPUS</h5>
-              <p className="first_subtext fw-bold text-white">
+              <p className="first_subtext fw-bold">
                 EMPOWERING FUTURE LEADERS.
               </p>
               <a href="#">
-                <button
-                  type="submit"
-                  className="submit d-block text-center m-auto"
-                >
+                <button type="submit" className="submit d-block m-auto">
                   Your experience feedback
                 </button>
               </a>
               <a href="#">
                 <button type="submit" className="notice mt-3">
+                  {" "}
                   Notice board
                 </button>
               </a>
             </div>
           </div>
-          <div className="carousel-item h-100">
+          <div className="carousel-item">
+            <img src={carousel1} className="d-block w-100" alt="img" />
             <div className="carousel-caption">
               <h5 className="fw-bold">GLOBAL DIGITAL CAMPUS</h5>
-              <p className="first_subtext fw-bold text-white">
+              <p className="first_subtext fw-bold">
                 EMPOWERING FUTURE LEADERS.
               </p>
               <a href="#">
-                <button
-                  type="submit"
-                  className="submit d-block text-center m-auto"
-                >
+                <button type="submit" className="submit d-block m-auto">
                   Your experience feedback
                 </button>
               </a>
               <a href="#">
                 <button type="submit" className="notice mt-3">
+                  {" "}
                   Notice board
                 </button>
               </a>
             </div>
           </div>
         </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          imga-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">
+            Previou className="first_subtext fw-bold"s
+          </span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+// import 'swiper/swiper-bundle.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
